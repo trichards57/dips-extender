15d56cc706f629f1e3d80747900b2d6c9cdad93b
Tony Richards <trichards57@gmail.com>
2018-03-21 19:13:21 +0000

    Removed make green bits.

--- inject.js
+++ inject.js
@@ -1,16 +1,6 @@
-document.body.style.backgroundColor = "green";
-
 const bodyFrame = document.getElementById("bodyiframe");
 
 bodyFrame.onload = function() {
-  makeGreen(bodyFrame.contentDocument);
+  console.log(bodyFrame.contentWindow.location.href);
 };
 
-makeGreen(bodyFrame.contentDocument);
-makeGreen(document);
-
-function makeGreen(doc) {
-  if (doc) {
-    doc.body.style.backgroundColor = "green";
-  }
-}
